9b7106538dbbf77f66bb29aec527c34c5155a749
djames912 <dwj@lowfatmail.com>
2013-03-27 19:58:47 -0600

    Changed the configuration files around associated with the connectdb().  This setup actually seems to work reliably.

---
+++ includes/dbconfig.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file contains an example setup of the dbconnect().  It is an example only
+ * and should be copied into dbconfig.php.local and then have the appropriate
+ * variables set to correctly communicate with the database.
+ */
+
+/*
+ * This function creates a connection to the database and returns a connection
+ * resource.  It accepts no arguments.
+ */
+function dbconnect()
+{
+  // Database credentials:
+  $dbHost = "changeme";
+  $dbName = "changeme";
+  $dbUser = "changeme";
+  $dbPass = "changeme";
+  try
+  {
+    $connection = "mysql:host=" . $dbHost . "; dbname=" . $dbName;
+    $dbcon = new PDO($connection, $dbUser, $dbPass, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    return $dbcon;
+  }
+  catch (PDOException $exception)
+  {
+    echo "<p>Unable to connect to the database</p>";
+    echo "<p>Please check your database installation/setup and try again.";
+    $dbcon['RSLT'] = "1";
+    $dbcon['MSSG'] = $exception->getMessage();
+  }
+}
+?>
--- includes/functions.php
+++ includes/functions.php
@@ -4,24 +4,4 @@
  * This file contains all the major functions used by this application.
  */
 
-/*
- * This function creates a connection to the database and returns a connection
- * resource.  It accepts no arguments.
- */
-function dbconnect()
-{
-  try
-  {
-    $connection = "mysql:host=" . $dbHost . "; dbname=" . $dbName;
-    $dbcon = new PDO($connection, $dbUser, $dbPass, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    return $dbcon;
-  }
-  catch (PDOException $exception)
-  {
-    echo "<p>Unable to connect to the database</p>";
-    echo "<p>Please check your database installation/setup and try again.";
-    $dbcon['RSLT'] = "1";
-    $dbcon['MSSG'] = $exception->getMessage();
-  }
-}
 ?>
