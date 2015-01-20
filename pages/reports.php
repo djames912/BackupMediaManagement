e7efc0a6bf941a4ff13e1b05663577f42dd299a3
djames912 <dwj@lowfatmail.com>
2015-01-20 16:15:35 -0700

    Started working on the reports page.

--- pages/reports.php
+++ pages/reports.php
@@ -1,4 +1,5 @@
 <div id="reports" class="page_content" style="">
+   <form method="POST" action="#">
   <div class="form1">
     <div align="center">
          <label class="title">Media History</label>
@@ -7,12 +8,13 @@
       <label class="spn">Media ID</label>
     <form id="indmediahistory" class="form1">
       <input class="iput" type="text">
+      </div>
     <button class="ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only green-button" value="Submit" role="button" aria-disabled="false">
       <span class="ui-button-text">Submit</span>
     </button>
     </form>
     </div>
-  </div>
+   </form>
   <div class="results"></div>
 </div>
 
